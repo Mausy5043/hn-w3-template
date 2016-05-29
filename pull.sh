fbb45a0c44570c34b0d2af8b58fa7c5986393d78
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-29 17:14:29 +0200

    20160529T1714

--- pull.sh
+++ pull.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # This script is used to download the Grav `user/pages/` directory
-HERE=$(dirname $0)
 
-pushd $HERE/..
-  lftp -f get.lftp
-  ls -al
-popd
+lftp -f get.lftp
+ls -al
--- push.sh
+++ push.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 # This script is used to upload the Grav `user/pages/` directory to the hosting site.
-HERE=$(dirname $0)
 
-pushd $HERE/..
-  lftp -f put.lftp
-popd
+lftp -f put.lftp
